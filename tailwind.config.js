283a4df9bf39fbf58173258bde684c9ba712340a
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-05-26 09:51:34 +0600

    Next Project setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      satoshi: ["Satoshi", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      "primary-orange": "#FF5722",
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         satoshi: ['Satoshi', 'sans-serif'],
+//         inter: ['Inter', 'sans-serif'],
+//       },
+//       colors: {
+//         'primary-orange': '#FF5722',
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
